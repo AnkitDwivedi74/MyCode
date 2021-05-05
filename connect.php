cd73cfe9db60afd4dcc82ccdefad8e9c858ecfb5
AnkitDwivedi74 <83722116+AnkitDwivedi74@users.noreply.github.com>
2021-05-05 22:48:13 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+    $FristName = $_POST['Fname'];
+    $LasttName = $_POST['Lname'];
+    $Email = $_POST['Ename'];
+    $MonthlyAdvertisingBudget = $_POST['Fname'];
+    $PhoneNumber = $_POST['Pname'];
+
+    //database connection
